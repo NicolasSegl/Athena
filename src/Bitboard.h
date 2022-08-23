947fa6df74f3886af07f43c04be601f64a8208b4
Nicolas <nseglenieks@gmail.com>
2022-08-22 20:38:37 -0400

    Cleaned up old code

--- src/Bitboard.h
+++ src/Bitboard.h
@@ -34,27 +34,6 @@ namespace BB
 		RANK_EIGHTH,
 	};
 
-	namespace Constants
-	{
-		/* predefined bitboards for piece starting positions (little endian file-rank mapping) */
-
-		// white pieces
-		const Bitboard cWPawnsStartBB	= 65280;
-		const Bitboard cWRooksStartBB	= 129;
-		const Bitboard cWKnightsStartBB = 66;
-		const Bitboard cWBishopsStartBB = 36;
-		const Bitboard cWQueensStartBB  = 8;
-		const Bitboard cWKingStartBB    = 16;
-
-		// black pieces
-		const Bitboard cBPawnsStartBB   = 71776119061217280;
-		const Bitboard cBRooksStartBB   = 9295429630892703744;
-		const Bitboard cBKnightsStartBB = 4755801206503243776;
-		const Bitboard cBBishopsStartBB = 2594073385365405696;
-		const Bitboard cBQueensStartBB  = 576460752303423488;
-		const Bitboard cBKingStartBB    = 1152921504606846976;
-	}
-
     extern Bitboard boardSquares[64];
     extern Bitboard fileClear[8]; // the vertical
     extern Bitboard rankClear[8]; // the horizontal
